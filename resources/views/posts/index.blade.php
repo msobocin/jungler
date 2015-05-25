7316de355e60e0c4e70728ccc642344eb9c8c77a
krm3l0 <mbecerra@jaumebalmes.net>
2015-05-25 19:58:01 +0200

    Post comments maquetacion

--- resources/views/posts/index.blade.php
+++ resources/views/posts/index.blade.php
@@ -7,22 +7,22 @@
 
             <!--        <h2>Posts</h2>-->
             <div class="row">
-               
-
-                    @if (!Auth::guest())
-
-                    {!! Form::model(new App\Post, ['route' => ['posts.store'], 'class'=>'']) !!}
-
-                    @include('posts/partials/_form', ['submit_text' => 'Create Post'])
-                    {!! Form::close() !!}
-
-                    @endif
 
 
-                    @if ( !$posts->count() )
-                    You have no post
-                    @else
-                </div>
+                @if (!Auth::guest())
+
+                {!! Form::model(new App\Post, ['route' => ['posts.store'], 'class'=>'']) !!}
+
+                @include('posts/partials/_form', ['submit_text' => 'Create Post'])
+                {!! Form::close() !!}
+
+                @endif
+
+
+                @if ( !$posts->count() )
+                You have no post
+                @else
+            </div>
 
             <div class="media comentario">
 
@@ -40,55 +40,58 @@
                     </h4>
                     <p>  {{ $post->content }}</p>
                     <div class="responder">
-                        <a href="" data-toggle="tooltip" data-placement="bottom" title="Responder"><span class="glyphicon glyphicon-pencil"></span></a> &nbsp; &nbsp; <a href="{{ route('posts.show', $post->slug) }}" data-toggle="tooltip" data-placement="bottom" title="Ver post"><span class="glyphicon glyphicon-eye-open" aria-hidden="true"></span> </a>
-                        &nbsp; &nbsp; <a class="" data-toggle="collapse" href="#{{$post->slug}}" aria-expanded="false" aria-controls="collapseExample" data-placement="bottom" title="Ver comentarios"><div class="glyphicon glyphicon-collapse-down">   </div>
+                        <a  href="#{{$post->slug}}" data-toggle="collapse"  aria-expanded="false" aria-controls="collapseExample" data-placement="bottom" title="Responder"><span class="glyphicon glyphicon-pencil"></span></a> &nbsp; &nbsp;
+                        <a href="{{ route('posts.show', $post->slug) }}" data-toggle="tooltip" data-placement="bottom" title="Ver post"><span class="glyphicon glyphicon-eye-open" aria-hidden="true"></span> </a>
+                        &nbsp; &nbsp;
 
-                        </a>
 
+                        @if ($post->comments->count()>0)
+                        <a class="" data-toggle="collapse" href="#{{$post->slug}}" aria-expanded="false" aria-controls="collapseExample" data-placement="bottom" title="Ver comentarios"><div class="glyphicon glyphicon-collapse-down">   </div> 
+                        </a>   
+                        @endif
                         <div class="collapse" id="{{$post->slug}}">
                             <div class="well">
                                 @if ($post->comments->count()>0)
 
 
-                                @foreach( $post->comments as $comment )
+                                    @foreach( $post->comments as $comment )
 
-                                <a class="pull-left" href=''>   <img class="img-circle" src="{{ asset('img/dede.jpg') }}" width="45"></a>
-                                <div class="media-body ">
-                                    <h4 class="media-heading">
-                                        {{ $comment->user->name }}<small >{{ $comment -> created_at }}</small></h4>
-                                    <p class="text-center"><p>{{ $comment->body }}</p>
-                                </div>
-                                <hr />
+                                        <a class="pull-left" href=''>   <img class="img-circle" src="{{ asset('img/dede.jpg') }}" width="45"></a>
+                                        <div class="media-body ">
+                                            <h4 class="media-heading">
+                                                {{ $comment->user->name }}<small >{{ $comment -> created_at }}</small></h4>
+                                            <p class="text-center"><p>{{ $comment->body }}</p>
+                                        </div>
+                                        <hr />
 
+                                     @endforeach
 
+                                    @if (!Auth::guest())    
 
-                                <!--<li><a href="{{ route('posts.show', $post->slug) }}">{{ $comment->body }} </a> {{ $comment -> created_at }} user: {{ $comment->user->name }}</li>-->
-                                @endforeach
-                                <!--</ul>-->
-                                @if (!Auth::guest())
+                                        {!! Form::model(new Lanz\Commentable\Comment, ['route' => ['posts.comments.store', $post->slug], 'class'=>'' ,'id'=>"commet-$post->id"]) !!}
+                                        @include('posts/partials/_form_comment', ['submit_text' => 'Create Comment'])
+                                        {!! Form::close() !!}
 
-                                {!! Form::model(new Lanz\Commentable\Comment, ['route' => ['posts.comments.store', $post->slug], 'class'=>'']) !!}
-                                @include('posts/partials/_form_comment', ['submit_text' => 'Create Comment'])
-                                {!! Form::close() !!}
+                                    @endif
 
-                                @endif
+                                @else 
+                              
+                                        @if (!Auth::guest())
 
-                                @else <p>No hay comentarios</p>
-                                @if (!Auth::guest())
+                                        {!! Form::model(new Lanz\Commentable\Comment, ['route' => ['posts.comments.store', $post->slug], 'class'=>'' ,'id'=>"commet-$post->id"]) !!}
+                                        @include('posts/partials/_form_comment', ['submit_text' => 'Create Comment'])
+                                        {!! Form::close() !!}
 
-                                {!! Form::model(new Lanz\Commentable\Comment, ['route' => ['posts.comments.store', $post->slug], 'class'=>'']) !!}
-                                @include('posts/partials/_form_comment', ['submit_text' => 'Create Comment'])
-                                {!! Form::close() !!}
-
-                                @endif
+                                        @endif
                                 @endif
                             </div>
                         </div>
 
+
                     </div>
 
                 </div>
-                <hr> 
+                <hr /> 
                 <!--<br />-->
                 <!--<li><a href="{{ route('posts.show', $post->slug) }}">{{ $post->content }} </a> {{ $post -> created_at }} user: {{ $post->user->name }}</li>-->
                 @endforeach
